af29bd28ec3054bb12bb992e4e3694baf35cfa71
Miriam Lerma <miriamjlerma@gmail.com>
2022-07-05 15:28:14 +0200

    Debe ser rda

---
+++ R/German_coast.R
@@ -0,0 +1,18 @@
+#' Shapefile
+#'
+#' Shapefile from German Coast
+#'
+#' @docType data
+#'
+#' @usage data(German_coast)
+#'
+#' @format Shapefile
+#'
+#' @keywords datasets
+#'
+#' @references To-do
+#'
+#'
+#' @examples
+#' data(German_coast)
+"German_coast"
