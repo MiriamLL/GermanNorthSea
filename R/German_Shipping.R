f2f127c54faf2f1c5525aa7b6fed60ca4fe39abc
Miriam Lerma <miriamjlerma@gmail.com>
2022-07-07 14:40:47 +0200

    Add shipping

---
+++ R/German_Shipping.R
@@ -0,0 +1,18 @@
+#' Shapefile
+#'
+#' Shapefile from German_Shipping
+#'
+#' @docType data
+#'
+#' @usage data(German_Shipping)
+#'
+#' @format Shapefile
+#'
+#' @keywords datasets
+#'
+#' @references To-do
+#'
+#'
+#' @examples
+#' data(German_Shipping)
+"German_Shipping"
