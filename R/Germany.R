dee122ad21f6ae54cb61aca906968fa1f2263d18
Miriam Lerma <miriamjlerma@gmail.com>
2022-08-08 17:15:22 +0200

    Added Germany only country shape

---
+++ R/Germany.R
@@ -0,0 +1,18 @@
+#' Shapefile
+#'
+#' Shapefile from Germany
+#'
+#' @docType data
+#'
+#' @usage data(Germany)
+#'
+#' @format Shapefile
+#'
+#' @keywords datasets
+#'
+#' @references To-do
+#'
+#'
+#' @examples
+#' data(Germany)
+"Germany"
