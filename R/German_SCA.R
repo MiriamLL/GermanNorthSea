8dd2755286490e977c8846fe7cae016e29dce7cc
Miriam Lerma <miriamjlerma@gmail.com>
2023-04-26 17:05:19 +0200

    Add special conservation area

---
+++ R/German_SCA.R
@@ -0,0 +1,18 @@
+#' Shapefile
+#'
+#' Shapefile from German Special Conservation Areas
+#'
+#' @docType data
+#'
+#' @usage data(German_SCA)
+#'
+#' @format Shapefile
+#'
+#' @keywords datasets
+#'
+#' @references To-do
+#'
+#'
+#' @examples
+#' data(German_SCA)
+"German_SCA"
