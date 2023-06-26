89d151798189665e317099875189b4fc9da84be5
Miriam Lerma <miriamjlerma@gmail.com>
2023-06-26 11:11:22 +0200

    add grids

---
+++ R/grid10x10_3035.R
@@ -0,0 +1,10 @@
+#' Contains a sf object with a grid from the North Sea of 10 x 10
+#' A total of 1424 observations of 2 variables
+#' @docType data
+#' @usage data(grid10x10_3035)
+#' @format A grid 10x10 of the north sea
+#' @keywords datasets
+#' @references Lerma et al. 2023
+#' @examples
+#' data(grid10x10_3035)
+"grid510x10_3035"
---
+++ R/grid5x5_3035.R
@@ -0,0 +1,10 @@
+#' Contains a sf object with a grid from the North Sea of 5 x 5
+#' A total of 4080 observations of 2 variables
+#' @docType data
+#' @usage data(grid5x5_3035)
+#' @format A grid 5x5 of the north sea
+#' @keywords datasets
+#' @references Lerma et al. 2023
+#' @examples
+#' data(grid5x5_3035)
+"grid5x5_3035"
