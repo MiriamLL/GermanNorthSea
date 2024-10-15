e10d85d8dc80c07c7aac2e09a17ce41e871501ac
Miriam Lerma <miriamjlerma@gmail.com>
2024-10-15 17:49:37 +0200

    include data, update package

---
+++ R/German_distancecoast.R
@@ -0,0 +1,18 @@
+#' Shapefile
+#'
+#' Shapefile from German Distance to Coast
+#'
+#' @docType data
+#'
+#' @usage data(German_distancecoast)
+#'
+#' @format Shapefile
+#'
+#' @keywords datasets
+#'
+#' @references To-do
+#'
+#'
+#' @examples
+#' data(German_distancecoast)
+"German_distancecoast"
