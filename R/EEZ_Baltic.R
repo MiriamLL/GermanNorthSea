0d9231381b54bacf26ce1abba6a1b67834770344
Miriam Lerma <miriamjlerma@gmail.com>
2025-08-29 14:43:21 +0200

    update add EEZ_Baltic

---
+++ R/EEZ_Baltic.R
@@ -0,0 +1,18 @@
+#' Shapefile
+#'
+#' Shapefile from EEZ_Baltic
+#'
+#' @docType data
+#'
+#' @usage data(EEZ_Baltic)
+#'
+#' @format Shapefile
+#'
+#' @keywords datasets
+#'
+#' @references To-do
+#'
+#'
+#' @examples
+#' data(EEZ_Baltic)
+"EEZ_Baltic"
