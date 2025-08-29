de948b433e19bc9a120395f478b75d33d4d81436
Miriam Lerma <miriamjlerma@gmail.com>
2025-08-29 13:07:55 +0200

    include EEZ_North

---
+++ R/EEZ_North.R
@@ -0,0 +1,18 @@
+#' Shapefile
+#'
+#' Shapefile from EEZ_North
+#'
+#' @docType data
+#'
+#' @usage data(EEZ_North)
+#'
+#' @format Shapefile
+#'
+#' @keywords datasets
+#'
+#' @references To-do
+#'
+#'
+#' @examples
+#' data(EEZ_North)
+"EEZ_North"
